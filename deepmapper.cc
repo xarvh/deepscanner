e4042b49773b0a06b14a667d6726126caaefdf0b
xarvh <francesco.orsenigo@gmail.com>
2015-12-06 12:16:45 +1100

    Increase orbital resolution

--- deepmapper.cc
+++ deepmapper.cc
@@ -33,7 +33,8 @@ Complex nextOrbit(Complex z, double a)
  * Searches for the first zero that gets close enough to the orbit.
  */
 void nearestZero(double r, double i, double alpha, int* orbitLength, int* closestZeroId) {
-    double squaredDistanceThreshold = 0.001 * 0.001;
+    double distanceThreshold = 1e-10;
+    double squaredDistanceThreshold = distanceThreshold * distanceThreshold;
 
     Complex zeroA[3];
 
