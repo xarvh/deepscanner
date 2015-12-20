844932d4ffdc9bc621ec144a1894141045f6377f
xarvh <francesco.orsenigo@gmail.com>
2015-12-20 22:26:14 +1100

    Allow Colorise to select shades via command line

--- colorise.cc
+++ colorise.cc
@@ -14,13 +14,16 @@ typedef struct {
 
 
 
-Pixel firstZeroColorScale(int value) {
+/* ===========================================================================
+ * TRANSFORMS
+ */
+Pixel white(int value) {
     Pixel p = {255, 255, 255};
     return p;
 }
 
 
-Pixel secondZeroColorScale(int value) {
+Pixel StockholmRed(int value) {
     Pixel p = {0, 0, 0};
 
     p.r = pow(2, 8 * (value / 256.));
@@ -35,36 +38,37 @@ Pixel secondZeroColorScale(int value) {
 }
 
 
-Pixel thirdZeroColorScale(int value) {
-    Pixel p = {255, 255, 255};
-    return p;
-}
+typedef Pixel (* Transform)(int);
+Transform transforms[] = {
+    white,
+    StockholmRed
+};
 
 
+/* ===========================================================================
+ * MAIN
+ */
 int main(int argc, char** argv) {
 
     int bufferSize = 1024*1024;
     Pixel bf[bufferSize];
 
-    FILE* in = fopen(argv[1], "rb");
-    FILE* out = fopen(argv[2], "wb");
+    if (argc < 6) {
+        fprintf(stderr, "%s RedTransform BlueTransform GreenTransform inutFile outputFile\n", argv[0]);
+        return -1;
+    }
+
+    Transform rt = transforms[atoi(argv[1])];
+    Transform gt = transforms[atoi(argv[2])];
+    Transform bt = transforms[atoi(argv[3])];
+
+    FILE* in = fopen(argv[4], "rb");
+    FILE* out = fopen(argv[5], "wb");
     while (int r = fread(bf, sizeof(Pixel), bufferSize, in)) {
         for (Pixel* p = bf; p - bf < r; p++) {
-            /*
-            Pixel black = {0, 0, 0}, white = {255, 255, 255}, red = {255, 0, 0};
-            int x = 0;
-            if (p->r) x++;
-            if (p->g) x++;
-            if (p->b) x++;
-
-            if (x == 0) *p = black;
-            else if (x == 1) *p = white;
-            else *p = red;
-            */
-
-            if (p->r) *p = firstZeroColorScale(p->r);
-            else if (p->g) *p = secondZeroColorScale(p->g);
-            else if (p->b) *p = thirdZeroColorScale(p->b);
+            if (p->r) *p = rt(p->r);
+            else if (p->g) *p = gt(p->g);
+            else if (p->b) *p = bt(p->b);
         }
         fwrite(bf, sizeof(Pixel), r, out);
     }
